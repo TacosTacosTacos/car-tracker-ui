a07d8dafa9f162cf4239fc0074ed2e984dc94efd
tacostacostacos <david.epstein@libertymutual.com>
2017-11-21 15:16:26 -0500

    Added unauth logic
    Added logic to kick out anyone without a token

--- app/routes/user-dashboard.js
+++ app/routes/user-dashboard.js
@@ -2,5 +2,12 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
-  flashMessages: Ember.inject.service()
+  flashMessages: Ember.inject.service(),
+  beforeModel () {
+    if (!this.get('auth.credentials.token')) {
+      this.transitionTo('/sign-in');
+      this.get('flashMessages')
+      .danger('You must be authenticated to access this page.');
+    }
+  }
 });
