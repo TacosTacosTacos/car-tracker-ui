f3446182acea574374412639575fb76f00e5ca68
tacostacostacos <david.epstein@libertymutual.com>
2017-11-23 20:59:05 -0500

    Started creation of Signup P3 form
    The form now has the basic signup features, but doesn't create the car
    record in the database.  This can't be done until after I create the
    associated API request.  I'll finish this work in a different branch.

--- app/routes/sign-up-p3.js
+++ app/routes/sign-up-p3.js
@@ -1,4 +1,32 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  flashMessages: Ember.inject.service(),
+  credentials: storageFor('auth'),
+  signupstorage: storageFor('sign-up'),
+
+// TODO: Submit request for creating car record in the database
+  actions: {
+    signUp (credentials) {
+      this.get('auth').signUp(credentials)
+      .then(() => this.get('auth').signIn(credentials))
+      .then(() => this.get('auth.credentials').set('hideConfetti', false))
+      .then(() => this.transitionTo('user-dashboard'))
+      .then(() => {
+        this.get('flashMessages')
+        .success('Congratulations on becoming a new user!');
+      })
+      .catch(() => {
+        this.get('flashMessages')
+        .danger('An unexpected problem occured. Please try again.');
+      });
+    },
+  },
+  beforeModel () {
+    if (this.get('auth.credentials.token')) {
+      this.transitionTo('user-dashboard');
+    }
+  }
 });
