b01e656e0d06b1c7eb2649d48975b5ec8edf98ba
tacostacostacos <david.epstein@libertymutual.com>
2017-11-23 20:45:23 -0500

    Created the second signup form
    The signupp2 form is setup
    The model text field is temporary until I can figure out how to do the
    call to the associated AJAX service
    Stores the form data locally

---
+++ app/components/model-form.js
@@ -0,0 +1,21 @@
+import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
+
+export default Ember.Component.extend({
+  signupstorage: storageFor('sign-up'),
+  modelform: {},
+  actions: {
+    submit () {
+      // Store the entered data
+      this.get('signupstorage').set('carModel', this.get('modelform.carModel'));
+      this.get('signupstorage').set('carLabel', this.get('modelform.carLabel'));
+      this.get('signupstorage').set('mileage', this.get('modelform.mileage'));
+      // Navigate to the next form
+      this.sendAction('submit');
+    },
+  },
+  willDestroyElement () {
+    this.set('modelform', {});
+  },
+
+});
--- app/routes/sign-up-p2.js
+++ app/routes/sign-up-p2.js
@@ -1,19 +1,23 @@
 import Ember from 'ember';
+import { storageFor } from 'ember-local-storage';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
-  model () {
-    return this.get('store').findAll('model');
-  },
+  signupstorage: storageFor('sign-up'),
+  // model () {
+  //   return this.get('store').findAll('model');
+  // },
   actions: {
     submit () {
       // Transition to the next signup screen
-      this.transitionTo('sign-up-p2')
+      this.transitionTo('sign-up-p3')
     },
   },
   beforeModel () {
     if (this.get('auth.credentials.token')) {
       this.transitionTo('user-dashboard');
+    } else if (!this.get('signupstorage.nhtsa_make_id')) {
+      this.transitionTo('sign-up-p1');
     }
   }
 });
