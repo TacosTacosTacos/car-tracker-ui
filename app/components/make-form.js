42e93d1a0efdab0a3356ee72ad4878bc4bda0363
tacostacostacos <david.epstein@libertymutual.com>
2017-11-24 14:17:02 -0500

    Saved Make ID
    Went through a roundabout way to save the form ID to the store.  I will
    want to revisit this logic later since I shouldn't need to do

--- app/components/make-form.js
+++ app/components/make-form.js
@@ -8,12 +8,19 @@ export default Ember.Component.extend({
     submit () {
       if(!this.get('makeform.nhtsa_make_id')) {
         // error handling for the make dropdown
-        console.log(this);
         this.get('flashMessages')
         .warning('You must enter a Car Make');
         this.$('#carMake').focus();
       } else {
         // Store the entered data
+        var myModel =  this.get('model');
+        console.log(myModel.content[0]._data.nhtsa_make_id)
+
+        var filteredMake = myModel.content.filter((internalModel) => {
+         return internalModel._data.nhtsa_make_id == this.get('makeform.nhtsa_make_id')
+       })[0].id;
+
+        this.get('signupstorage').set('make_id', filteredMake);
         this.get('signupstorage').set('nhtsa_make_id', this.get('makeform.nhtsa_make_id'));
         this.get('signupstorage').set('year', this.get('makeform.year'));
         // Navigate to the next form
