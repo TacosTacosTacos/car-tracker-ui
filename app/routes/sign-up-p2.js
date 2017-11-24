038b1234552e1da047d5b10d72b840a66c7965be
tacostacostacos <david.epstein@libertymutual.com>
2017-11-23 19:54:22 -0500

    Started to create the car model signup page
    Created a model for car models
    Created a service file...likely needs to be deleted...

--- app/routes/sign-up-p2.js
+++ app/routes/sign-up-p2.js
@@ -1,4 +1,19 @@
 import Ember from 'ember';
 
 export default Ember.Route.extend({
+  auth: Ember.inject.service(),
+  model () {
+    return this.get('store').findAll('model');
+  },
+  actions: {
+    submit () {
+      // Transition to the next signup screen
+      this.transitionTo('sign-up-p2')
+    },
+  },
+  beforeModel () {
+    if (this.get('auth.credentials.token')) {
+      this.transitionTo('user-dashboard');
+    }
+  }
 });
