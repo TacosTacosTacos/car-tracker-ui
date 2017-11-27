554c6d980168939276afff71fa1c3b9212d71ae4
tacostacostacos <david.epstein@libertymutual.com>
2017-11-27 11:34:37 -0500

    Created Mileage form Populated Mileage details Need to add form validation and submit action

--- app/routes/editmileage.js
+++ app/routes/editmileage.js
@@ -1,8 +1,15 @@
 import Ember from 'ember';
+import RSVP from 'rsvp';
 
 export default Ember.Route.extend({
   auth: Ember.inject.service(),
   flashMessages: Ember.inject.service(),
+  model(params) {
+  return RSVP.hash({
+    cars: this.get('store').findAll('car'),
+    mileage: this.get('store').findRecord('mileage', params.mileage_id)
+  });
+},
   beforeModel () {
     if (!this.get('auth.credentials.token')) {
       this.transitionTo('/sign-in');
