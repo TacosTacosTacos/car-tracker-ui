a5892ab10fb4da449fec0bdcc152d80abf271b05
tacostacostacos <david.epstein@libertymutual.com>
2017-11-29 18:52:49 -0500

    Switched Godzilla to github storage

--- app/components/marketing-logged-out.js
+++ app/components/marketing-logged-out.js
@@ -6,7 +6,7 @@ export default Ember.Component.extend({
       this.get('flashMessages')
       .danger('GODZILLA ATTACK IN PROGRESS!!!!');
       this.$('div').addClass('vibrate-3')
-      this.$('#sound_element').html("<audio autoplay><source src=http://soundbible.com/mp3/Godzilla_Roar-Marc-1912765428.mp3 type=audio/mpeg></audio>");
+      this.$('#sound_element').html("<audio autoplay><source src=https://raw.githubusercontent.com/TacosTacosTacos/car-tracker-ui/nhtsa/app/konami/Godzilla_Roar-Marc-1912765428.mp3 type=audio/mpeg></audio>");
     }
   }
 });
