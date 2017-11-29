a6007757cae3180720f47fd82faaa5774cec18c9
tacostacostacos <david.epstein@libertymutual.com>
2017-11-29 10:58:10 -0500

    Started implementation of Konami Code
    Added Raptor assets

--- app/components/marketing-logged-out.js
+++ app/components/marketing-logged-out.js
@@ -1,4 +1,10 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+  actions: {
+    doCoolStuff () {
+      this.get('flashMessages')
+      .success('Cheats Enabled');
+    }
+  }
 });
