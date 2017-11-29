54a244556a786bc235f64052a23262895f61d0de
tacostacostacos <david.epstein@libertymutual.com>
2017-11-29 18:58:36 -0500

    Adjusted secret text

--- app/components/marketing-logged-out.js
+++ app/components/marketing-logged-out.js
@@ -4,7 +4,7 @@ export default Ember.Component.extend({
   actions: {
     doCoolStuff () {
       this.get('flashMessages')
-      .danger('GODZILLA ATTACK IN PROGRESS!!!!');
+      .danger('Did you hear that? It must be Godzilla!!!! Sign up for CarTracker while you still can.');
       this.$('div').addClass('vibrate-3')
       this.$('#sound_element').html("<audio autoplay><source src=https://raw.githubusercontent.com/TacosTacosTacos/car-tracker-ui/nhtsa/app/konami/Godzilla_Roar-Marc-1912765428.mp3 type=audio/mpeg></audio>");
     }
