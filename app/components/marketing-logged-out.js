672187f2cc6b5d56b20cb155c8d1e31ff589d25f
tacostacostacos <david.epstein@libertymutual.com>
2017-11-29 19:30:01 -0500

    Added missing semi-colon
    Ember was yelling at me...so I listened to it for once

--- app/components/marketing-logged-out.js
+++ app/components/marketing-logged-out.js
@@ -5,7 +5,7 @@ export default Ember.Component.extend({
     doCoolStuff () {
       this.get('flashMessages')
       .danger('Did you hear that? It must be Godzilla!!!! Sign up for CarTracker while you still can.');
-      this.$('div').addClass('vibrate-3')
+      this.$('div').addClass('vibrate-3');
       this.$('#sound_element').html("<audio autoplay><source src=https://raw.githubusercontent.com/TacosTacosTacos/car-tracker-ui/nhtsa/app/konami/Godzilla_Roar-Marc-1912765428.mp3 type=audio/mpeg></audio>");
     }
   }
